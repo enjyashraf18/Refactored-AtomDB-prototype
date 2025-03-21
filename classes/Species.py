33cd43e7a4de0256fc631e4efffed8c677ab26dc
Enjy <enjyashraf18@gmail.com>
2025-03-21 02:10:45 +0200

    Species sample class is done

--- classes/Species.py
+++ classes/Species.py
@@ -1,20 +1,44 @@
-import json
-import os
 from dataclasses import dataclass, asdict, field
 from typing import Dict, Any, Optional
 
-
-
 @dataclass
 class Species:
-    dataset: str
-    elem: str
-    atnum: int
-    nelec: int
-    nspin: int
-    nexc: int
-    energy: float
-    mult: int
-    spinpol: int = field(default=1)
-    doc_id: Optional[int] = field(default=None)
+    """
+    Species is a class for representing atomic and ionic species.
+    """
 
+    def __init__(self, dataset, fields, spinpol=1, doc_id=None):
+        super().__init__(value=fields, doc_id=doc_id)
+        self._dataset = dataset.lower()
+        self._spinpol = spinpol
+        self._fields = fields
+
+        # Instead of manually assigning each field (for ex: self.elem = fields["elem"])
+        for field_name, value in fields.items():
+            setattr(self, field_name, value)
+
+    def to_dict(self):
+        # if no Conversion Required anymore
+        # return self._fields
+        return asdict(self._fields)
+
+
+
+    def get_data(self):
+        """simply returns self._fields dict"""
+        return self._fields
+
+    @classmethod
+    def from_dict(cls, dataset, data):
+        """create a species instance from a dictionary"""
+        return cls(dataset, data)
+
+
+    @property
+    def dataset(self):
+        """return the dataset name"""
+        return self._dataset
+
+    @property
+    def charge(self):
+        return self._fields.atnum - self._fields.nelec
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from classes.ZipFileStorage import ZipFileStorage
+
+
+
+
 import json
 import os
-from tinydb import TinyDB
 from classes.Species import Species
-from classes.ZipFileStorage import ZipFileStorage
 from classes.SpeciesDB import SpeciesDB
+import logging
 
-
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     fields = {
@@ -17,5 +23,12 @@ if __name__ == "__main__":
         "energy": -0.5,
         "mult": 2,
     }
+
+    species = Species(dataset="default", **fields)
+    db_handler = SpeciesDB()
+    db_handler.dump(species)
+    loaded_species = db_handler.load("H", 2, 0)
+    logger.info(f"Loaded species: {loaded_species}")
+    db_handler.close()
 
 
